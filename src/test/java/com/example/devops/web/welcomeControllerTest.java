1575f9db5cf634c5a82dd5e6ed97774d150864bb
Saurabh Sheel <sheel.gta@gmail.com>
2020-08-13 00:02:53 -0400

    Updated with SCM and dis change for artifact.

---
+++ src/test/java/com/example/devops/web/welcomeControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.devops.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class welcomeControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testWelcome() throws Exception {
+		this.mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(model().attribute("course", "DevOps"));
+	}
+
+}
